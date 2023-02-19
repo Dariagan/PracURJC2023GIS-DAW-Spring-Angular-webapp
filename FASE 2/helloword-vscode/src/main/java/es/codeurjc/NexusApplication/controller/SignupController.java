ed3727794bcb9761255c02abc13ba3e6b38e9b78
Stefano Tomasini Hoefner <seomasini@gmail.com>
2023-02-19 12:15:03 +0100

    A lot of html and css fixes
    created SignupController class
    
    linked register-login switch buttons correctly to their respective page switch
    
    removed all the figmapx from all css

---
+++ FASE 2/helloword-vscode/src/main/java/es/codeurjc/NexusApplication/controller/SignupController.java
@@ -0,0 +1,44 @@
+package es.codeurjc.NexusApplication.controller;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import es.codeurjc.NexusApplication.service.UserService;
+import es.codeurjc.NexusApplication.model.User;
+
+@Controller
+public class SignupController {
+
+    @Autowired
+    private UserService userService;
+    
+    @GetMapping("/signup")
+    public String showSignupForm() {
+
+        return "signup/loginpage";
+    }
+   
+    /* 
+    @PostMapping("/login")
+    public String processLoginForm(@RequestParam String username, @RequestParam String password, Model model) {
+        Optional<User> user = userService.getUserByUsername(username);
+
+        if (user.isEmpty()) {
+            model.addAttribute("error", "Username does not exist");
+            return "login/loginpage";
+        } 
+        else if (!user.get().getEncodedPassword().equals(password)) {
+            model.addAttribute("error", "Password is incorrect");
+            return "login/loginpage";
+        } 
+        else {
+            model.addAttribute("username", username);
+            return "login/loginpage";
+        }
+    }*/
+}
