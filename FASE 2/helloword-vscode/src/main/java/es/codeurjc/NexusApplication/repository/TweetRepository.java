bebcea4b56edb7c17faf86de410e51b750062553
Alberto <alberyoshi@gmail.com>
2023-02-28 09:25:20 +0100

    Created HandlerNotFound and modified Repository

--- FASE 2/helloword-vscode/src/main/java/es/codeurjc/NexusApplication/repository/TweetRepository.java
+++ FASE 2/helloword-vscode/src/main/java/es/codeurjc/NexusApplication/repository/TweetRepository.java
@@ -3,6 +3,8 @@ package es.codeurjc.NexusApplication.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.h2.mvstore.Page;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import es.codeurjc.NexusApplication.model.Tweet;
@@ -11,7 +13,11 @@ import es.codeurjc.NexusApplication.model.User;
 public interface TweetRepository extends JpaRepository<Tweet, Long>{
 
     Optional<Tweet> findById(Long id);
-    List<Tweet> findByOwner(User user);
+    List<Tweet> findByOwnerUserOrderByDateAsc(User user);//if doesnt work, just put: findByOwnerOrderByDateAsc
     List<Tweet> findByTag(String tag);
-    
+    List<Tweet> findByOwner();
+    List<Tweet> findAllOrderByDateAsc();
+    List<Tweet> findFirst10ByOwner(User user);
+    List<Tweet> findFirst10ByOwnerOrderByDateAsc(User user);
+    //Page<Tweet> findByOwner(User user, Pageable page);
 }
