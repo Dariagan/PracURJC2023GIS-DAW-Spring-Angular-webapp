92aface5f161a0a509de2228c2710a125b02139a
DenisUrjc <DenisUrjc@proton.me>
2023-03-11 18:45:13 +0100

    feat: 404 page

---
+++ FASE 2/helloword-vscode/src/main/java/es/codeurjc/backend/controller/PageNotFoundController.java
@@ -0,0 +1,13 @@
+package es.codeurjc.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageNotFoundController {
+    /*
+    @RequestMapping("/error")
+    public String pageNotFound() {
+        return "404";
+    }*/
+}
